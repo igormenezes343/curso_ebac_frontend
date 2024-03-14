6bc33ddd5f6b6592539403eb822209a58c07d725
igormenezes343 <igorpm343@gmail.com>
2024-03-13 21:52:22 -0300

    update

--- main.js
+++ main.js
@@ -1,28 +1,26 @@
 const form = document.getElementById('form-numerico');
 
 form.addEventListener('submit',function(e){
-    e.preventDefault();
-
-
+    e.preventDefault();   
     const campoAinput = document.getElementById('campo-a');
     const campoBinput = document.getElementById('campo-b');
 
     const campoA = parseFloat(campoAinput.value);
     const campoB = parseFloat(campoBinput.value);
 
-    const mensagemVerdade = `O numero ${campoB} é maior que o numero ${campoA}`;
-    const mensagemFalso = `O numero ${campoB} é menor que o numero ${campoA}`;
-
-    if (campoA < campoB) {
-        const containerVerdade = document.querySelector('.mensagen-verdade')
-        containerVerdade.innerHTML = mensagemVerdade;
-        containerVerdade.style.display = 'block'
-        
-    }
-    else{
-        const containerFalso = document.querySelector('.menesagen-falso')
+    const mensagemFalso = `Tente outra vez`;
+    const mensagemVerdade = ` Esta correto o numero ${campoB} é maior que o numero ${campoA}`;
+    
+    if(campoA >= campoB){
+        const containerFalso = document.querySelector('.mensagen-falso');
         containerFalso.innerHTML = mensagemFalso;
-        containerFalso.style.display = 'block'
-        
+        containerFalso.style.display = 'block';
+        document.querySelector('.mensagen-verdade').style.display = 'none';
     }
-})
+    else{     
+        const containerVerdade = document.querySelector('.mensagen-verdade');
+        containerVerdade.innerHTML = mensagemVerdade;
+        containerVerdade.style.display = 'block';
+        document.querySelector('.mensagen-falso').style.display = 'none';
+    }
+});
